1b15993a615b8c5e1ab8c5ebfc541becd423db8c
venkata432 <46181835+venkata432@users.noreply.github.com>
2019-01-07 19:29:32 +0530

    New files aded

---
+++ DemoForPipeline/src/main/java/com/quest/demo/DemoForPipeline/App.java
@@ -0,0 +1,13 @@
+package com.quest.demo.DemoForPipeline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
